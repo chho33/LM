f03895e8d1a6e8bc813a2fa7fe8a4ed26467eecb
Nate <jojotenya@gmail.com>
2018-10-16 18:08:18 +0800

    add inference

--- data_utils.py
+++ data_utils.py
@@ -42,19 +42,23 @@ def build_word_dict(filename,start_index=4):
     gc.collect()
     return word_dict
 
-def build_dataset(filename, word_dict):
-    with open(filename, "r") as f:
-        lines = f.readlines()
-        data = map(lambda s: s.strip().split()[:max_document_len], lines)
-        data = map(lambda s: ["<bos>"] + s + ["<eos>"], data)
-        data = map(lambda s: [word_dict.get(w, word_dict["<unk>"]) for w in s], data)
+def build_dataset(filename, word_dict, pad=True):
+    if isinstance(filename, str): 
+        with open(filename, "r") as f:
+            lines = f.readlines()
+            data = map(lambda s: s.strip().split()[:max_document_len], lines)
+    elif isinstance(filename, (list, np.ndarray, np.generic)):
+        data = filename
+        data = map(lambda s: s.strip().split()[:max_document_len], data)
+    data = map(lambda s: ["<bos>"] + s + ["<eos>"], data)
+    data = map(lambda s: [word_dict.get(w, word_dict["<unk>"]) for w in s], data)
+    if pad: 
         data = map(lambda d: d + (max_document_len +2 - len(d)) * [word_dict["<pad>"]], data)
-
     return data
 
 def get_dataset(filename):
     with open(filename, "r") as f: 
-        rows = [row.strip().split() for row in f.readlines()]
+        rows = [list(map(lambda x:int(x),row.strip().split())) for row in f.readlines()]
     return rows
 
 def get_word_dict(filename):
@@ -75,9 +79,31 @@ def batch_iter(inputs, batch_size, num_epochs):
 def data_loader(filename,batch_size=64,epochs=5,shuffle=True):
     dataset = tf.data.TextLineDataset([filename])
     if shuffle:
-        dataset = dataset.shuffle(buffer_size=100000)
+        dataset = dataset.shuffle(buffer_size=1000000)
     dataset = dataset.batch(batch_size)
     dataset = dataset.repeat(epochs)
     iterator = dataset.make_one_shot_iterator()
     next_element = iterator.get_next()
     return next_element 
+
+def map_get_words(txts,kind="char",return_type="str"):
+    if isinstance(txts, str):
+        with open(txts,"r") as f:
+            txts = [row.strip() for row in f.readlines()]
+    jieba = None
+    if kind == "word":
+        import jieba_fast as jieba
+        jieba.initialize()
+        jieba.load_userdict("dict_fasttext.txt")
+    txts = list(map(lambda txt:get_words(txt,kind,return_type,jieba), txts)) 
+    return txts  
+
+def get_words(txt,kind="char",return_type="str",jieba=None):
+    if kind == "word":
+        result = jieba.lcut(txt)[:max_document_len]
+        #result = list(filter(lambda x:len(x.strip())>0,result))
+    elif kind == "char":
+        result = [t for t in txt][:max_document_len]
+    if return_type == "str":
+        result = ' '.join(result)
+    return result
